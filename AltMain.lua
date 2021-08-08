3b0aad17cd0cdadf7e88d0a4edae1edee5440df3
GelatekWasTaken <87724188+GelatekWasTaken@users.noreply.github.com>
2021-08-08 17:07:57 +0200

    Update AltMain.lua

--- AltMain.lua
+++ AltMain.lua
@@ -12,13 +12,17 @@ local UIStyle = "Midnight"
 ]]--
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
-local Window = Library.CreateLib("Gelatek Hub V1.15", UIStyle)
+local Window = Library.CreateLib("Gelatek Hub V1.2", UIStyle)
 local Tab = Window:NewTab("Home")
 local Tab2 = Window:NewTab("Scripts")
-
-local HomePage = Tab:NewSection("Home")
+local Tab3 = Window:NewTab("LocalPlayer")
+local Tab4 = Window:NewTab("Hat List")
+local HomePage = Tab:NewSection("Home - Thanks for using the hub!")
 local Section = Tab2:NewSection("Free Scripts")
-local Section2 = Tab2:NewSection("Hat Scripts")
+local Section2 = Tab2:NewSection("Converted Hat Scripts")
+local Section3 = Tab2:NewSection("Fixed Hat Scripts")
+local Section4 = Tab3:NewSection("COMING SOON")
+local Section5 = Tab4:NewSection("COMING SOON")
 HomePage:NewButton("Copy Discord Invite", "funny button", function()
     setclipboard("https://discord.gg/Azb6TzVTqF")
 end)
@@ -59,11 +63,15 @@ end)
 Section:NewButton("Gale Fighter", "funny puncc", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/GaleFighter"))()
 end)
+
+Section:NewButton("Ender", "funny puncc v2", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/Ender"))()
+end)
 Section:NewButton("Krystal Dance", "pog", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/Krystal"))()
 end)
 
-Section2:NewButton("Minigun", "5154115297 | REEEEEEE", function()
+Section3:NewButton("Minigun", "5154115297 | REEEEEEE", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/AltMinigun"))()
 end)
 
@@ -71,11 +79,11 @@ Section2:NewButton("Yellow Lightning Blade", "5644668053 | swing swing and fling
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/LightningBlade"))()
 end)
 
-Section2:NewButton("Neptunian V", "Greatsword | aaaaaaaaa", function()
+Section3:NewButton("Neptunian V", "Greatsword | aaaaaaaaa", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/Neptunian"))()
 end)
 
-Section2:NewButton("Sniper", "5063578607 | fe bush script when", function()
+Section3:NewButton("Sniper", "5063578607 | fe bush script when", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/Sniper"))()
 end)
 
@@ -83,14 +91,25 @@ Section2:NewButton("Carnage", "5063578607 | flying sniper (more cool)", function
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/Carnage"))()
 end)
 
-Section2:NewButton("Dual Ultima", "Blades | piss blade but 2 swords", function()
+Section3:NewButton("Dual Ultima", "Blades | piss blade but 2 swords", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/DualUltima"))()
 end)
 
-Section2:NewButton("Elio Biasto", "4962455546 | same as sniper like how", function()
+Section3:NewButton("Elio Biasto", "4962455546 | same as sniper like how", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/ElioBiasto"))()
 end)
 
-Section2:NewButton("Achromatic", "i wonder how many edits this has.", function()
+Section3:NewButton("Achromatic", "i wonder how many edits this has.", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/Achromatic"))()
+end)
+
+Section2:NewButton("GLITCHY - Abyss Eye", "4820152700 | srs, its bad but works.", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/AbyssEye"))()
+end)
+Section3:NewButton("Abyss Eye", "Greatsword | No bugs, everything is here.", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/BetterAbyssEye"))()
+end)
+
+Section3:NewButton("Christmas Neptunian", "4492451337 | Faster Neptunian with sweet.", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/ChristmasNeptunian"))()
 end)
