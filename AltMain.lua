e32f47d1a5b2cf0a874363c4262e922016728115
GelatekWasTaken <87724188+GelatekWasTaken@users.noreply.github.com>
2021-08-09 15:42:43 +0200

    Update AltMain.lua

--- AltMain.lua
+++ AltMain.lua
@@ -10,19 +10,38 @@ local UIStyle = "Midnight"
     Sentinel
     Synapse
 ]]--
+
+----------------------------------------\\\\\\\\\\\\\\\
+-- Setup
+----------------------------------------///////////////
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
-local Window = Library.CreateLib("Gelatek Hub V1.25", UIStyle)
+local Window = Library.CreateLib("Gelatek Hub V1.2", UIStyle)
 local Tab = Window:NewTab("Home")
 local Tab2 = Window:NewTab("Scripts")
-local Tab3 = Window:NewTab("LocalPlayer")
-local Tab4 = Window:NewTab("Hat List")
+local Tab3 = Window:NewTab("Hat Scripts")
+local Tab4 = Window:NewTab("LocalPlayer")
+local Tab5 = Window:NewTab("Hat List")
+
 local HomePage = Tab:NewSection("Home - Thanks for using the hub!")
+
 local Section = Tab2:NewSection("Free Scripts")
-local Section2 = Tab2:NewSection("Converted Hat Scripts")
-local Section3 = Tab2:NewSection("Fixed Hat Scripts")
-local Section4 = Tab3:NewSection("COMING SOON")
-local Section5 = Tab4:NewSection("COMING SOON")
+
+local Section2 = Tab3:NewSection("Converted Hat Scripts")
+
+local Section3 = Tab3:NewSection("Fixed Hat Scripts")
+
+local Section4 = Tab4:NewSection("Humanoid")
+
+local Section4More = Tab4:NewSection("Client")
+
+local Section4Tools = Tab4:NewSection("Fast Scripts")
+
+local Section5 = Tab5:NewSection("Click To Copy Hat ID")
+
+----------------------------------------\\\\\\\\\\\\\\\
+-- Home
+----------------------------------------///////////////
 HomePage:NewButton("Copy Discord Invite", "funny button", function()
     setclipboard("https://discord.gg/Azb6TzVTqF")
 end)
@@ -57,20 +76,24 @@ Gale Fighter/Sonic:
 Free
 ]]--
 
+----------------------------------------\\\\\\\\\\\\\\\
+-- Free scripts
+----------------------------------------///////////////
 Section:NewButton("Sonic", "funny script", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/Sonic"))()
 end)
 Section:NewButton("Gale Fighter", "funny puncc", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/GaleFighter"))()
 end)
-
 Section:NewButton("Ender", "funny puncc v2", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/Ender"))()
 end)
 Section:NewButton("Krystal Dance", "pog", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/Krystal"))()
 end)
-
+----------------------------------------\\\\\\\\\\\\\\\
+-- Hat scripts
+----------------------------------------///////////////
 Section3:NewButton("Minigun", "5154115297 | REEEEEEE", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/AltMinigun"))()
 end)
@@ -99,11 +122,11 @@ Section3:NewButton("Elio Biasto", "4962455546 | same as sniper like how", functi
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/ElioBiasto"))()
 end)
 
-Section3:NewButton("Achromatic", "i wonder how many edits this has.", function()
+Section3:NewButton("Achromatic v2", "i wonder how many edits this has.", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/Achromatic"))()
 end)
 
-Section2:NewButton("GLITCHY - Abyss Eye", "4820152700 | srs, its bad but works.", function()
+Section2:NewButton("Own Convert - Abyss Eye", "4820152700 | srs, its kinda bad but works.", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/AbyssEye"))()
 end)
 Section3:NewButton("Abyss Eye", "Greatsword | No bugs, everything is here.", function()
@@ -113,3 +136,99 @@ end)
 Section3:NewButton("Christmas Neptunian", "4492451337 | Faster Neptunian with sweet.", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/ChristmasNeptunian"))()
 end)
+
+Section2:NewButton("Chips", "4047554959 | those chips are from brazil!", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/Chips"))()
+end)
+
+Section3:NewButton("Updated Achromatic", "Newer Version of Achromatic", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GelatekWasTaken/real/main/AchromaticUpdate"))()
+end)
+
+
+
+----------------------------------------\\\\\\\\\\\\\\\
+-- LocalPlayer
+----------------------------------------///////////////
+
+local Humanoid = game.Players.LocalPlayer.Character.Humanoid
+Section4:NewSlider("WalkSpeed", "Changes Speed of your character.", 200, 1, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    Humanoid.WalkSpeed = s
+end)
+
+
+Section4:NewSlider("Script Speed", "Changes Speed of your character in script.", 200, 1, function(e) -- 500 (MaxValue) | 0 (MinValue)
+    game.Workspace.GelatekReanimation.Humanoid.WalkSpeed = e
+end)
+
+
+Section4:NewSlider("Jump Power", "Changes Jump Height of your character.", 200, 1, function(j) -- 500 (MaxValue) | 0 (MinValue)
+    Humanoid.JumpPower = j
+end)
+
+Section4More:NewSlider("Camera FOV", "Changes Your Camera FOV. (def = 75)", 120, 30, function(b) -- 500 (MaxValue) | 0 (MinValue)
+    Game.Workspace.Camera.FieldOfView = b
+end)
+
+Section4More:NewButton("Instant Death", "death", function()
+    Humanoid:Destroy()
+end)
+
+Section4Tools:NewButton("Infinity Yield Admin", "Simple admin with 50+ Commands", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+end)
+
+Section4Tools:NewButton("Dex Explorer V3", "Client Sided Explorer.", function()
+loadstring(game:GetObjects("rbxassetid://418957341")[1].Source)()
+end)
+----------------------------------------\\\\\\\\\\\\\\\
+-- Hat Scripts ID
+----------------------------------------///////////////
+Section5:NewButton("Neptunian V", "copy button", function()
+    setclipboard("4794315940")
+end)
+
+Section5:NewButton("Elio Biasto", "copy button", function()
+    setclipboard("4962455546")
+end)
+
+Section5:NewButton("Achromatics", "copy button", function()
+    setclipboard("4794315940,4458601937,4524991457,4820152700,4506945409,4315489767")
+end)
+
+Section5:NewButton("Sniper/Carnage", "copy button", function()
+    setclipboard("45063578607")
+end)
+
+Section5:NewButton("Yellow Lightning Blade", "copy button", function()
+    setclipboard("5644668053")
+end)
+
+Section5:NewButton("Minigun", "copy button", function()
+    setclipboard("5154115297")
+end)
+
+Section5:NewButton("Minigun", "copy button", function()
+    setclipboard("4492451337")
+end)
+
+Section5:NewButton("Converted Abyss Eye", "copy button", function()
+    setclipboard("4820152700")
+end)
+
+Section5:NewButton("Fixed Abyss Eye", "copy button", function()
+    setclipboard("4506945409")
+end)
+
+Section5:NewButton("Chips (FREE)", "copy button", function()
+    setclipboard("417457461")
+end)
+
+Section5:NewButton("Dual Ultima", "copy button", function()
+    setclipboard("4820152700,4524991457")
+end)
+
+Section5:NewButton("Christmas Neptunian", "copy button", function()
+    setclipboard("4492451337")
+end)
+
